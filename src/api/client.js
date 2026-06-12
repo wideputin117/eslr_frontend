a08f0c26d60217139d7c240e35e51771ea794cb2
Manish <manishsaingupta@smartdatainc.net>
2026-06-12 12:43:24 +0530

    added elr backend api

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://eslr-backend.onrender.com/api'
 class APIClient {
     constructor() {
         Object.defineProperty(this, "client", {
